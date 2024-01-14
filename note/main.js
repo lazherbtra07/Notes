fba0f79b7a7281fe270d42aa9baffef96d3e011a
LazherBtr <108631379+lazherbtra07@users.noreply.github.com>
2024-01-14 14:25:43 +0400

    Add files via upload

--- note/main.js
+++ note/main.js
@@ -1,4 +1,36 @@
 const niteContainer = document.querySelector(".note-contanier ")
 const createBtn = document.querySelector(".btn")
+let notes = document.querySelectorAll(".input-box")
 
-let notes = document.querySelector(". input-box")
+function showNote(){
+  niteContainer.innerHTML = localStorage.getItem("notes")
+}
+showNote();
+
+function updateStorage() {
+  localStorage.setItem("notes", niteContainer.innerHTML)
+}
+
+createBtn.addEventListener("click", () =>{
+  let inputBox = document.createElement("p")
+  let img = document.createElement("img")
+  inputBox.className = "input-box"
+  inputBox.setAttribute("contenteditable", "true")
+  img.src = "./images/delete.png"
+  niteContainer.appendChild(inputBox).appendChild(img)
+})
+
+niteContainer.addEventListener("click", function(e) {
+  if(e.target.tagName === "IMG"){
+    e.target.parentElement.remove()
+    updateStorage()
+  }
+  else if(e.target.tagName === "P"){
+    notes = document.querySelectorAll(".input-box")
+    notes.forEach(nt =>{
+      nt.onkeyup = function(){
+        updateStorage()
+      }
+    })
+  }
+})
